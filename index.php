2c2a92b36fc290942628d6b543ff7d8a2a0d40f6
Rachid Ahitass <rachid.ahitass@gmail.com>
2020-04-09 02:30:10 +0100

    adding more in depth to the loop
    adding some semantic tags, meta infos and content with link title

--- index.php
+++ index.php
@@ -1,34 +1,49 @@
 <?php get_header(); ?>
 <div class="container index">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-9">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                <h3 class="panel-title">Blog Posts</h3>
+                    <h3 class="panel-title">Blog Posts</h3>
                 </div>
             <?php if( have_posts() ): ?> 
+            <?php $i=0 ?>
             <?php while(have_posts()):the_post() ?>
                 <div class="panel-body">
                     <div class="row">
-                        <div class="col-md-3">
+                        <article <?php post_class('post-'.$i) ?>>
                             <?php if( has_post_thumbnail() ): ?>
-                                <?php the_post_thumbnail() ?>
+                                <div class="col-md-3">
+                                    <?php $attr = array(
+                                        'class' => 'img-responsive' 
+                                    ) ?>
+                                    <?php the_post_thumbnail('', $attr); ?>
+                                </div>
                             <?php endif; ?>
-                        </div>
-                        <div class="col-md-9">
-                            <?php the_title('<h3>','</h3>'); ?>
-                        </div>
+                                <div class="<?php echo has_post_thumbnail() ? 'col-md-9' : 'col-md-12'  ?>">
+                                    <?php the_title('<h3 class="post-title"><a href="'.get_the_permalink().'">','</a></h3>'); ?>
+                                    <ul class="meta list-inline well well-sm">
+                                        <li>Posted on: <?php the_time() ?></li>
+                                        <li>At: <?php the_date('F d, Y'); ?></li>
+                                        <li>By: <?php the_author(); ?></li>
+                                    </ul>
+                                    <div class="content">
+                                    <?php the_excerpt(); ?>
+                                    </div>
+                                </div>
+                        </article>
                     </div>
-                </div>
-            <?php endwhile; ?>
-            <?php endif; ?>
-            </div>
-        </div>
-    <?php if( is_active_sidebar('sidebar') ): ?>
-        <div class="col-md-4">
+                </div><!-- End Panel Body -->
+                <?php $i++ ?>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            </div><!-- End Panel -->
+        </div><!-- End Content Area -->
+        <?php if( is_active_sidebar('sidebar') ): ?>
+        <div class="col-md-3">
             <?php dynamic_sidebar('sidebar'); ?>
-        </div>
-    <?php endif; ?>
-    </div>
-</div>
+        </div><!-- End Sidebar -->
+        <?php endif; ?>
+    </div><!-- End Top Row -->
+</div><!-- End Top Container -->
 <?php get_footer(); ?>
