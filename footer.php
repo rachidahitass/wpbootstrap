99b05468d7cfde72cd3c10dc9889536b5c450fd4
Rachid Ahitass <rachid.ahitass@gmail.com>
2020-04-09 17:22:06 +0100

    Update footer.php
    addin some Bootstrap class utilities

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 <footer>
-    <p>&copy; <?php bloginfo('name'); ?>&ndash;<?php echo date('Y'); ?></p>
-    <?php wp_footer() ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            <p>&copy; <?php bloginfo('name'); ?>&ndash;<?php echo date('Y'); ?></p>
+            <?php wp_footer() ?>
+            </div>
+        </div>
+    </div>
  	</footer>
 </body>
 </html>
