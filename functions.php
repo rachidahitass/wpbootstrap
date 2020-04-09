585394a90dbc80706855bf1309744b546367a45f
Rachid Ahitass <rachid.ahitass@gmail.com>
2020-04-09 17:22:34 +0100

    Update functions.php
    Adding Bootstrap menu walker

--- functions.php
+++ functions.php
@@ -24,8 +24,13 @@ if ( ! function_exists( 'wpbootstrap_setup' ) ) :
 	 * as indicating support for post thumbnails.
 	 */
 	function wpbootstrap_setup() {
-
-		require_once('inc/class-wp-bootstrap-navwalker.php');
+		if ( ! file_exists( get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php' ) ) {
+			// file does not exist... return an error.
+			return new WP_Error( 'wp-bootstrap-navwalker-missing', __( 'It appears the wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+		} else {
+			// file exists... require it.
+			require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+		}
 		/**
 		 * Enable support for post thumbnails and featured images.
 		 */
