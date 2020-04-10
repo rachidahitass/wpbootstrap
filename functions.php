4acbd544e0ca94fd13e8ff9c64b1117e0e83fd35
Rachid Ahitass <rachid.ahitass@gmail.com>
2020-04-10 20:00:06 +0100

    Adding new custom css, and new callback funnction for comments

--- functions.php
+++ functions.php
@@ -71,6 +71,7 @@ if ( ! function_exists( 'wpbootstrap_setup' ) ) :
 		 **/
 		function wpbootstrap_scripts() {
 			wp_enqueue_style('bootstrapmin', '//stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css','','3.4.1', 'all');
+			wp_enqueue_style('wpbscustom', get_template_directory_uri() . '/css/wpbcss.css','','1.0', 'all');
 			wp_enqueue_style('fontawesome', '//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css','','4.7.0', 'all');
 			wp_enqueue_script('bootstrapjsmin', '//stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js',array('jquery'), '3.4.1', true);
 			wp_enqueue_script('wpbscript', get_template_directory_uri() . '/js/script.js','', '1.0', true);
@@ -92,6 +93,60 @@ if ( ! function_exists( 'wpbootstrap_setup' ) ) :
 		function add_new_class_list_categories($list){
 			$list = str_replace('cat-item', 'cat-item list-group-item', $list);
 			return $list;
+		}
+		// Add Comments
+		function add_theme_comments($comment, $args, $depth){
+			$GLOBALS['comment'] = $comment;
+			extract($args, EXTR_SKIP);
+			if ( 'div' === $args['style'] ) {
+				$tag       = 'div';
+				$add_below = 'comment';
+			} else {
+				$tag       = 'li';
+				$add_below = 'div-comment';
+			}?>
+			<<?php echo $tag; ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ); ?> id="comment-<?php comment_ID() ?>"><?php 
+			if ( 'div' != $args['style'] ) { ?>
+				<div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
+			} ?>
+				<div class="comment-author vcard"><?php 
+					if ( $args['avatar_size'] != 0 ) {
+						echo get_avatar( $comment, $args['avatar_size'] ); 
+					} 
+					printf( __( '<cite class="fn">%s</cite> <span class="says">says:</span>' ), get_comment_author_link() ); ?>
+				</div><?php 
+				if ( $comment->comment_approved == '0' ) { ?>
+					<em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php 
+				} ?>
+				<div class="comment-meta commentmetadata">
+					<a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>"><?php
+						/* translators: 1: date, 2: time */
+						printf( 
+							__('%1$s at %2$s'), 
+							get_comment_date(),  
+							get_comment_time() 
+						); ?>
+					</a><?php 
+					edit_comment_link( __( '(Edit)' ), '  ', '' ); ?>
+				</div>
+		
+				<?php comment_text(); ?>
+		
+				<div class="reply"><?php 
+						comment_reply_link( 
+							array_merge( 
+								$args, 
+								array( 
+									'add_below' => $add_below, 
+									'depth'     => $depth, 
+									'max_depth' => $args['max_depth'] 
+								) 
+							) 
+						); ?>
+				</div><?php 
+			if ( 'div' != $args['style'] ) : ?>
+				</div><?php 
+			endif;
 		}
 		add_filter('wp_list_categories', 'add_new_class_list_categories');
 		function wpbootstrap_init_widget() {
