066845ced9d11b25b00b7bf0e032a3db92e33e49
Rachid Ahitass <rachid.ahitass@gmail.com>
2020-04-10 20:00:14 +0100

    Update comments.php

--- comments.php
+++ comments.php
@@ -17,11 +17,30 @@
         <?php endif; ?>
         <ul class="row comments-lists">
             <?php
-            $args = array(
-                'avatar-size' => '64',
-                'callback'  => 'add_theme_comments'
-            );
-                wp_list_comments();
+                wp_list_comments(
+                    array(
+                        'avatar_size' => 48,
+                        'callback'  => 'add_theme_comments'
+                    )
+                );
             ?>
         </ul>
+    <?php if(!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type(), 'comments')) : ?>
+        <p class="no-comments"><?php _e('Comments are closed.', 'dazzling'); ?></p>
+    <?php endif; ?>
 </div>
+<hr>
+<?php
+    $comments_args = array(
+    // change the title of send button
+    'label_submit'=>'Send',
+    // change the title of the reply section
+    'title_reply'=>'Write a Reply or Comment',
+    // remove "Text or HTML to be displayed after the set of comment
+    //fields"
+    'comment_notes_after'=>'',
+    // redefine your own textarea (the comment body)
+    'comment_field'=>'<p class="comment-form-comment"><label for="comment">' ._x('Comment', 'noun') . '</label><br/>
+    <textarea class="form-control" id="comment" name="comment" aria-required="true"></textarea></p>',
+    );
+    comment_form($comments_args);
